be4d069a654b735a3f990380f018a5e4f856c13d
Shaira <mallari.shaira31@gmail.com>
2024-07-20 12:59:20 -0500

    Delete unnecesary files

--- App.js
+++
@@ -1,9 +0,0 @@
-const heading = React.createElement(
-    "h1", 
-    {id: "heading"}, 
-    "Hello WOrld from React"
-);
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("header"));
-
-root.render(heading);
